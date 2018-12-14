2bed84c289ca8be7529242afa35819b4b293fe27
luluchang11053 <luluchang11053@gmail.com>
2018-12-14 16:52:37 +0800

    order table switch

--- src/components/OrderItemToPrepare.vue
+++ src/components/OrderItemToPrepare.vue
@@ -4,10 +4,10 @@
   <table id="completeTable" >
     <thead>
       <tr>
-        <th colspan="3" class="tableName">Complete</th>
-        <th colspan="3">Hot drink</th>
-        <th colspan="3">Cold kitchen</th>
-        <th colspan="3">Sides drink</th>
+        <th colspan="3" class="tableName" v-on:click="changeTable('completeTable')">Complete</th>
+        <th colspan="3" v-on:click="changeTable('hotTable')">Hot drink</th>
+        <th colspan="3" v-on:click="changeTable('coldTable')">Cold kitchen</th>
+        <th colspan="3" v-on:click="changeTable('sideTable')">Sides drink</th>
       </tr>
     </thead>
     <tbody>
@@ -26,66 +26,72 @@
   <table id="hotTable" >
     <thead>
       <tr>
-        <th colspan="3">Complete</th>
-        <th colspan="3" class="tableName">Hot drink</th>
-        <th colspan="3">Cold kitchen</th>
-        <th colspan="3">Sides drink</th>
+        <th colspan="3"  v-on:click="changeTable('completeTable')">Complete</th>
+        <th colspan="3" class="tableName" v-on:click="changeTable('hotTable')">Hot drink</th>
+        <th colspan="3" v-on:click="changeTable('coldTable')">Cold kitchen</th>
+        <th colspan="3" v-on:click="changeTable('sideTable')">Sides drink</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td colspan="4" height="100">Burger</td>
-        <td colspan="4" height="100"></td>
-        <td colspan="4" height="100"></td>
+        <td colspan="3" height="100">Burger</td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>        
       </tr>
       <tr>
-        <td colspan="4" height="100"></td>
-        <td colspan="4" height="100"></td>
-        <td colspan="4" height="100"></td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>        
       </tr>
     </tbody>
   </table>
   <table id="coldTable" >
     <thead>
       <tr>
-        <th colspan="3">Complete</th>
-        <th colspan="3">Hot drink</th>
-        <th colspan="3" class="tableName">Cold kitchen</th>
-        <th colspan="3">Sides drink</th>
+        <th colspan="3" v-on:click="changeTable('completeTable')">Complete</th>
+        <th colspan="3" v-on:click="changeTable('hotTable')">Hot drink</th>
+        <th colspan="3" class="tableName" v-on:click="changeTable('coldTable')">Cold kitchen</th>
+        <th colspan="3" v-on:click="changeTable('sideTable')">Sides drink</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td colspan="4" height="100">Burger</td>
-        <td colspan="4" height="100"></td>
-        <td colspan="4" height="100"></td>
+        <td colspan="3" height="100">Burger</td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>
       </tr>
       <tr>
-        <td colspan="4" height="100"></td>
-        <td colspan="4" height="100"></td>
-        <td colspan="4" height="100"></td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>
       </tr>
     </tbody>
   </table>
   <table id="sideTable" >
     <thead>
       <tr>
-        <th colspan="3">Complete</th>
-        <th colspan="3">Hot drink</th>
-        <th colspan="3">Cold kitchen</th>
-        <th colspan="3" class="tableName">Sides drink</th>
+        <th colspan="3" v-on:click="changeTable('completeTable')">Complete</th>
+        <th colspan="3" v-on:click="changeTable('hotTable')">Hot drink</th>
+        <th colspan="3" v-on:click="changeTable('coldTable')">Cold kitchen</th>
+        <th colspan="3" class="tableName" v-on:click="changeTable('sideTable')">Sides drink</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td colspan="4" height="100">Burger</td>
-        <td colspan="4" height="100"></td>
-        <td colspan="4" height="100"></td>
+        <td colspan="3" height="100">Burger</td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>
       </tr>
       <tr>
-        <td colspan="4" height="100"></td>
-        <td colspan="4" height="100"></td>
-        <td colspan="4" height="100"></td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>
+        <td colspan="3" height="100"></td>
       </tr>
     </tbody>
   </table>
@@ -121,6 +127,13 @@ export default {
     },
     cancelOrder: function () {
       // not implemented
+    },
+    changeTable: function (id) {
+      var tables = document.getElementsByTagName("table")
+      for(var i=0; i<tables.length; i++){
+        tables[i].style.display = "none";
+      }
+      document.getElementById(id).style.display = "table";
     }
   }
 }
@@ -130,6 +143,9 @@ export default {
     margin-left:auto; 
     margin-right:auto;
     display:none;
+  }
+  #completeTable {
+    display:table;
   }
   table, td, th{
     border: 1px solid black;
